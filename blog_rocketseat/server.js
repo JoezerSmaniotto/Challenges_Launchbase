67cd76a2165a8ef8aa2b2c353e46d0590a1cf08b
wendell-priebe <wendel_oliveira94@hotmail.com>
2020-09-27 19:39:56 -0300

    Curso Launchbase

--- blog_rocketseat/server.js
+++ blog_rocketseat/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjuckes = require('nunjucks')
+const blog = require('./data')
 
 const server = express()
 
@@ -7,14 +8,32 @@ server.use(express.static('public'))
 server.set("view engine","njk")
 
 nunjuckes.configure("views",{
-    express:server
+    express:server,
+    autoescape:false
 })
 
 server.get('/',function(req,res) {
-    return res.render('about')          // pagina inicial
+    const about ={
+        avatar_url:"https://cdn-images-1.medium.com/max/1200/1*TkXVfLTwsHdwpUEjGzdi9w.jpeg",
+        name:"RocketSeat",
+        title:"O que é Rocketseat?",
+        description:"O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500",
+        technologys:[{
+                name:"JavaScript"
+            },{
+                name:"NodeJS"
+            },{
+                name:"ReactJS"
+            },{
+                name:"React Native"
+            },{
+                name:"ES6"
+            }
+        ]}
+    return res.render('about',{about})          // pagina inicial
 })
 server.get('/courses',function(req,res){
-    return res.render('courses')        // pagina cursos
+    return res.render('courses',{posts:blog})        // pagina cursos
 })
 server.use(function(req,res){
     res.status(404).render('not-found') //pagina não encontrada
