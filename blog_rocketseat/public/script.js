82c1ddd8762c3050e4892abb5d307e7602877ace
wendell-priebe <wendel_oliveira94@hotmail.com>
2020-10-01 18:50:14 -0300

    Curso Launchbase

--- blog_rocketseat/public/script.js
+++ blog_rocketseat/public/script.js
@@ -1,3 +1,13 @@
+// =====VIDEOS=====
+const videos = document.querySelectorAll('.script_video')
+
+for(let video of videos){
+    video.addEventListener("click",function(){
+        const videoID = video.getAttribute("id")
+        window.location.href = `/video?id=${videoID}`
+    })
+}
+// =====BLOG=====
 const modalOverlay = document.querySelector('.modal_overlay')
 const cards = document.querySelectorAll('.card')  //querySelectorAll pega uma coleçao de itens
 
@@ -13,6 +23,7 @@ document.querySelector('#close').addEventListener("click",function(){
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector("iframe").src = ""
     modalOverlay.classList.remove('modal_overlay_full')
+    modalOverlay.querySelector('#open_in_full').innerHTML='open_in_full'
 })
 
 document.querySelector('#open_in_full').addEventListener("click",function(){
@@ -24,3 +35,4 @@ document.querySelector('#open_in_full').addEventListener("click",function(){
         modalOverlay.querySelector('#open_in_full').innerHTML='close_fullscreen'
     }
 })
+
--- blog_rocketseat/server.js
+++ blog_rocketseat/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nunjuckes = require('nunjucks')
-const blog = require('./data')
+const blog = require('./data_blog')
+const videos = require('./data_videos.js')
 
 const server = express()
 
@@ -9,10 +10,11 @@ server.set("view engine","njk")
 
 nunjuckes.configure("views",{
     express:server,
-    autoescape:false
+    autoescape:false,
+    noCache: true
 })
 
-server.get('/',function(req,res) {
+server.get('/',function(req,res) {                   //pagina inicial
     const about ={
         avatar_url:"https://cdn-images-1.medium.com/max/1200/1*TkXVfLTwsHdwpUEjGzdi9w.jpeg",
         name:"RocketSeat",
@@ -33,12 +35,27 @@ server.get('/',function(req,res) {
     return res.render('about',{about})          // pagina inicial
 })
 server.get('/courses',function(req,res){
-    return res.render('courses',{posts:blog})        // pagina cursos
+    return res.render('courses',{posts:blog})        // pagina de blog
+})
+server.get('/videos',function(req,res){
+    return res.render('videos',{videos})              //pagina de videos
+})
+server.get('/video',function(req,res){
+    const id = req.query.id
+    const video = videos.find(function(video){
+        if(video.id == id){
+            return true
+        }
+    })
+    if(!video){
+        return res.render('not-found')
+    }
+    return res.render('video',{video})
 })
 server.use(function(req,res){
     res.status(404).render('not-found') //pagina não encontrada
 })
 
 server.listen(5000, function(){
-    console.log('server is running')
+    console.log('server is running')    //avisa no terminal q o server ta funcionando
 })
